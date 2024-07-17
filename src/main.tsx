a4afcefe151930f99245d00ddc0e36b2cf33d330
Artur Toikka <arturtoikka@Arturs-MacBook-Pro.local>
2024-07-18 00:28:27 +0300

    hopefully last fix for now

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://arturtoikka.github.io/RapBattles/tonconnect-mainfest.json';
+const manifestUrl = 'https://arturtoikka.github.io/RapBattles/tonconnect-manifest.json';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
